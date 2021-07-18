3216beebec30dc76cad9638fef1d75038b741698
ANSHIKA <71966377+ANSHIKA1806@users.noreply.github.com>
2021-07-18 18:16:03 +0530

    Update Reverse Nodes in k-Group.java

--- Reverse Nodes in k-Group.java
+++ Reverse Nodes in k-Group.java
@@ -5,3 +5,14 @@ class Solution {
 		
         ListNode root = new ListNode(-1);
         root.next = head;
+	
+	int count = countNodes(head);
+        int reverseCount = count / k;
+        ListNode node = root;
+        while (reverseCount > 0) {
+            node = reverse(node, k);
+            reverseCount--;
+        }
+
+        return root.next;
+    }
