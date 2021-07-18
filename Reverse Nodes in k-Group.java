386017a11f47a7fcc6219f0439be6163ac5cdde2
ANSHIKA <71966377+ANSHIKA1806@users.noreply.github.com>
2021-07-18 18:15:35 +0530

    Update Reverse Nodes in k-Group.java

--- Reverse Nodes in k-Group.java
+++ Reverse Nodes in k-Group.java
@@ -1,2 +1,7 @@
 class Solution {
-    
+     public ListNode reverseKGroup(ListNode head, int k) {
+		
+        if (k == 1) return head;
+		
+        ListNode root = new ListNode(-1);
+        root.next = head;
