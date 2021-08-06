887bd9e736b4800a22bbacc83f400a1f2933dac1
ANSHIKA <71966377+ANSHIKA1806@users.noreply.github.com>
2021-08-06 23:05:09 +0530

    Update N-ary Tree Level Order Traversal.java

--- N-ary Tree Level Order Traversal.java
+++ N-ary Tree Level Order Traversal.java
@@ -1,1 +1,29 @@
-
+*/
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> values = new ArrayList<>();
+        
+        if (root == null) {
+            return values;
+        }
+        
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            List<Integer> temp = new ArrayList<>();
+            
+            while(n-- > 0) {
+                Node node = queue.remove();
+                temp.add(node.val);
+                List<Node> childrens = node.children;
+                queue.addAll(childrens);
+            }
+            
+            values.add(temp);
+        }
+        
+        return values;
+    }
+}
